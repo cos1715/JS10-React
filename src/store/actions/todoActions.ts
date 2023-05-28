3fbc38408a7c6bbf59d471cf39dec8d4834f2814
Taras Samoilenko <tsamoilenko97@gmail.com>
2023-05-28 11:23:42 +0300

    lesson-44

--- src/store/actions/todoActions.ts
+++ src/store/actions/todoActions.ts
@@ -4,17 +4,30 @@ export const ADD_TODO = "ADD_TODO";
 export const DELETE_TODO = "DELETE_TODO";
 export const UPDATE_TODO = "UPDATE_TODO";
 
-export const addTodo = (todo: ITodoItem) => {
+export interface IAddTodoAction extends IAction<ITodoItem> {
+  type: typeof ADD_TODO;
+}
+
+export interface IDeleteTodoAction extends IAction<ITodoItem["id"]> {
+  type: typeof DELETE_TODO;
+}
+
+export interface IUpdateTodoAction
+  extends IAction<{ id: ITodoItem["id"]; completed: ITodoItem["completed"] }> {
+  type: typeof UPDATE_TODO;
+}
+
+export const addTodo = (todo: ITodoItem): IAddTodoAction => {
   return { type: ADD_TODO, payload: todo };
 };
 
-export const deleteTodo = (id: ITodoItem["id"]): IAction<ITodoItem["id"]> => {
+export const deleteTodo = (id: ITodoItem["id"]): IDeleteTodoAction => {
   return { type: DELETE_TODO, payload: id };
 };
 
 export const updateTodo = (
   id: ITodoItem["id"],
   completed: ITodoItem["completed"]
-): IAction<{ id: ITodoItem["id"]; completed: ITodoItem["completed"] }> => {
+): IUpdateTodoAction => {
   return { type: UPDATE_TODO, payload: { id, completed } };
 };
--- src/store/reducers/todoReducer.ts
+++ src/store/reducers/todoReducer.ts
@@ -1,23 +1,40 @@
-import { ADD_TODO, DELETE_TODO, UPDATE_TODO } from "../actions/todoActions";
+import {
+  ADD_TODO,
+  DELETE_TODO,
+  UPDATE_TODO,
+  IAddTodoAction,
+  IDeleteTodoAction,
+  IUpdateTodoAction,
+} from "../actions/todoActions";
 import { IAction, IStore } from "../types";
+
+const addTodo = (state: IStore["todo"], action: IAddTodoAction) => {
+  return [...state, action.payload];
+};
+
+const deleteTodo = (state: IStore["todo"], action: IDeleteTodoAction) => {
+  return state.filter((todo) => todo.id !== action.payload);
+};
+
+const updateTodo = (state: IStore["todo"], action: IUpdateTodoAction) => {
+  return state.map((todo) => {
+    return todo.id === action.payload.id
+      ? { ...todo, completed: action.payload.completed }
+      : todo;
+  });
+};
 
 export const todoReducer = (
   state: IStore["todo"] = [],
-  action: IAction<any>
+  action: IAction<unknown>
 ) => {
-  console.log(state, action);
-
   switch (action.type) {
     case ADD_TODO:
-      return [...state, action.payload];
+      return addTodo(state, action as IAddTodoAction);
     case DELETE_TODO:
-      return state.filter((todo) => todo.id !== action.payload);
+      return deleteTodo(state, action as IDeleteTodoAction);
     case UPDATE_TODO:
-      return state.map((todo) => {
-        return todo.id === action.payload.id
-          ? { ...todo, completed: action.payload.completed }
-          : todo;
-      });
+      return updateTodo(state, action as IUpdateTodoAction);
     default:
       return state;
   }
