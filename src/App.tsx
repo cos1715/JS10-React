e926df586d2bf88b117b4cdddd6b8b46368a4dfa
Taras Samoilenko <tsamoilenko97@gmail.com>
2023-06-03 11:35:19 +0300

    project set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
 import { Provider } from "react-redux";
 import { store } from "./store/store";
-import { TodoList } from "components/todo-list";
-import { ThemeChanger } from "components/theme-changer/theme-changer";
-import { Products } from "components/products";
 
 import "./App.css";
 
 function App() {
   return (
-    // Використовуєм Provider щоб мати доступ до store в компонентах
     <Provider store={store}>
-      <div className="App">
-        <Products />
-        <ThemeChanger />
-        <TodoList />
-      </div>
+      <div className="App"></div>
     </Provider>
   );
 }
