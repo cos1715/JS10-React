32a2f000d52a37104f553cec855dc1024d1d7595
Taras Samoilenko <tsamoilenko97@gmail.com>
2023-06-03 11:04:28 +0300

    dnd

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import App from "./App";
-import App from "./react-dnd";
+import App from "./App";
+// import App from "./react-dnd";
 // import App from "./AlternativeApp";
 import "./index.css";
 
