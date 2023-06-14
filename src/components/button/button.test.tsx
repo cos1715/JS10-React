f7ef34c3ddc8a3b2cea6b7c05a3fb6b05dbdcb04
Taras Samoilenko <tsamoilenko97@gmail.com>
2023-06-14 18:33:28 +0300

    reset

--- src/components/button/button.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import { Button } from "./";
-
-test("renders learn react link", () => {
-  render(<Button title="Click me!" />);
-  const linkElement = screen.getByText(/Click me!/i);
-  expect(linkElement).toBeInTheDocument();
-});
